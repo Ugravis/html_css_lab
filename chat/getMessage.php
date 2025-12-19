fec75ae0ddde8eb0bb31e5b75aa6ccf6b9da8fa0
Ulysse Pennetier <upennetier@gmail.com>
2025-12-19 11:17:33 +0100

    feat(html): ajax script

--- chat/getMessage.php
+++ chat/getMessage.php
@@ -1,4 +1,8 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+
   $db_host = "localhost";
   $db_user = "root";
   $db_pass = "";
@@ -11,14 +15,14 @@
     $db_name
   );
 
-  $pseudo = $_GET['pseudo'];
+  $username = $_GET['username'];
   $message = $_GET['message'];
 
-  if ($pseudo && $message) {
-    $sql_req = "INSERT INTO chat (pseudo, message) VALUES ('$pseudo', '$message')";
+  if ($username && $message) {
+    $sql_req = "INSERT INTO chat (username, message) VALUES ('$username', '$message')";
 
     if ($connexion->query($sql_req)) {
-      echo "Success: message from {$pseudo}.";
+      echo "Success: message from {$username}.";
     } else {
       echo "Error: {$connexion->error}";
     }
